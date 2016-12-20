a33889c6aa21f04353df376e7a140142ec31ae22
yovannanetwork <yovannanetwork@gmail.com>
2016-12-20 08:39:42 -0600

    js suma de dos numeros

---
+++ main.js
@@ -0,0 +1,10 @@
+var num1, num2;
+
+num1 = prompt("por favor, ingresa un numero")
+num2 = prompt("por favor, ingresa un numero")
+
+resultado = parseInt(num1)+ parseInt(num2);
+
+alert(resultado);
+
+
